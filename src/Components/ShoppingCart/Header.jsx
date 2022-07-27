8d024a3185bb0389941e021813fa966ef674659d
kalkidan12 <kalget12@gmail.com>
2022-07-27 23:44:36 +0300

    first commit

---
+++ src/Components/ShoppingCart/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+function Header() {
+	return (
+		<nav calssName="">
+			<h2 className="nav-brand">Online Shop</h2>
+			<div className="cart-icon"></div>
+		</nav>
+	);
+}
+
+export default Header;
